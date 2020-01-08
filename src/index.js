3eff1e974d5b5b3210475ce501a307cb0b8c7169
HanfordWu <hanfordwu@gmail.com>
2020-01-08 14:53:04 -0500

    add auth listner and wrapped router

--- src/index.js
+++ src/index.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import 'semantic-ui-css/semantic.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import "semantic-ui-css/semantic.min.css";
+import firebase from "./firebase";
 
+class Root extends React.Component {
+  state = {};
 
-const Root = () => {
-    return ( 
-        <Router>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-            </Switch>
-        </Router>
-     );
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return (
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+    );
+  }
 }
- 
 
+const RootWithAuth = withRouter(Root);
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
