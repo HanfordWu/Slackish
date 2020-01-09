3a42292dbc9d56317960c2c01ed580e1ec1022d9
HanfordWu <hanfordwu@gmail.com>
2020-01-09 16:54:13 -0500

    Create Modal for Adding Channels

---
+++ src/components/SidePanel/Channels.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { Menu, Icon, Modal, Form, Input, Button } from "semantic-ui-react";
+
+class Channels extends React.Component {
+  state = {
+    channels: [],
+    modal: false,
+    channelName: "",
+    channelDetails: ""
+  };
+
+  closeModal = () => {
+    this.setState({ modal: false });
+  };
+
+  openModal = () => {
+      this.setState({ modal: true });
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { channels, modal } = this.state;
+    return (
+      <React.Fragment>
+        <Menu.Menu style={{ paddingBottom: "2em" }}>
+          <Menu.Item>
+            <span>
+              <Icon name="exchange" /> CHANNELS
+            </span>{" "}
+            ({channels.length}) <Icon name="add" onClick={this.openModal} />
+          </Menu.Item>
+        </Menu.Menu>
+        <Modal basic open={modal} onClose={this.closeModal}>
+          <Modal.Header>Add a Channel</Modal.Header>
+          <Modal.Content>
+            <Form>
+              <Form.Field>
+                <Input
+                  fluid
+                  label="Name of the Channel"
+                  name="channelName"
+                  onChange={this.handleChange}
+                />
+              </Form.Field>
+              <Form.Field>
+                <Input
+                  fluid
+                  label="About the Channel"
+                  name="channelDetails"
+                  onChange={this.handleChange}
+                />
+              </Form.Field>
+            </Form>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button color="green" inverted>
+              <Icon name="checkmark" />
+              Add
+            </Button>
+            <Button color="red" inverted onClick={this.closeModal} >
+              Cancel
+              <Icon name="remove" />
+            </Button>
+          </Modal.Actions>
+        </Modal>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Channels;
