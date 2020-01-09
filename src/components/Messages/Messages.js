50ad5aa4a0e19abf8b08a9546e59ee601c923c21
HanfordWu <hanfordwu@gmail.com>
2020-01-08 21:25:26 -0500

    app.js layout

---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SidePanel from '../SidePanel/SidePanel';
+
+class Messages extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>Messages</div>
+         );
+    }
+}
+ 
+export default Messages;
---
+++ src/components/SidePanel/SidePanel.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class SidePanel extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>SidePanel</div>
+         );
+    }
+}
+ 
+export default SidePanel;
